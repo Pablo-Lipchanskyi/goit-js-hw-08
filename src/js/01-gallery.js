5c65fe1047a04a642fb486f7b0e8c6f843144840
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-27 21:15:14 +0300

    library plus code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const mainGallery = document.querySelector(".gallery");
+const createdImages = galleryItemsCreate(galleryItems);
+mainGallery.insertAdjacentHTML('beforeend', createdImages);
+mainGallery.addEventListener("click", onClick);
 
+
+function onClick(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== "IMG") {
+        return
+    } 
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData : "alt",
+    captionsDelay : "250",
+})
+lightbox.open
+
+
+
+function galleryItemsCreate(galleryItems) {
+    return [...galleryItems].map(
+        ({ description, preview, original }) =>
+            `<a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            data-source = "${original}"
+            alt="${description}"/></a>
+            `).join('');
+}
 console.log(galleryItems);
