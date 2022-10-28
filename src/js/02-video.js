351c73838def816e60b1fe81c116d3a1792e1a45
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-28 20:59:05 +0300

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,10 @@
 import Player from '@vimeo/player';
-const iframe = document.querySelector(`iframe`);
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const playTime = function () {
-     localStorage.setItem(`Time`,  )
+const Onplay = function (event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds);
 }
-console.log(player)
+  
+player.on(`timeupdate`, throttle(Onplay,1000));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name=email]'),
+    textarea: document.querySelector('[name=message]')
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextInput);
+savedLocal()
 
+function onTextInput(event) {
+    const message = event.target.value;
+    localStorage.setItem('feedback-form', message)
+}
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem(message)
+    console.log("Jnghfdrf")
+}
+function savedLocal() {
+    const savedMessage = localStorage.getItem('feedback-form');
+    if (savedMessage) {
+        console.log(savedMessage);
+    }
+    
+}
