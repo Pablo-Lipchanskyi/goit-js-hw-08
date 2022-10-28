237eeb9a0ed6124a8b208863436fd34e7c713b56
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-28 14:48:00 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
-
+import Player from '@vimeo/player';
+const iframe = document.querySelector(`iframe`);
+const player = new Player(iframe);
+const playTime = function () {
+     localStorage.setItem(`Time`,  )
+}
+console.log(player)
