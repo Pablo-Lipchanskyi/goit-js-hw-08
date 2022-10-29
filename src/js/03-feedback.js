05ca40d8b086515c019e60b62b4239cd94b1b0a6
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-29 12:15:06 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,15 @@
 import throttle from "lodash.throttle";
-const STORAGE = 'feedback-form-state';
+const STORAGE_FORM = 'feedback-form-state';
+const STORAGE_MAIL = 'feedback-mail';
 const refs = {
     form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('[name=message]')
+    textarea: document.querySelector('[name=message]'),
+    mail: document.querySelector('[name=email]')
 };
 const formData = {};
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextInput, 500));
+refs.mail.addEventListener('input', throttle(onMailInput, 500));
 refs.form.addEventListener('input', e => {
     formData[e.target.name] = e.target.value;
     localStorage.setItem('Data', JSON.stringify(formData))
@@ -16,19 +19,28 @@ savedLocal()
 
 function onTextInput(event) {
     const message = event.target.value;
-    localStorage.setItem(STORAGE, message);
+    localStorage.setItem(STORAGE_FORM, message);
+    
+}
+function onMailInput(event) {
+    const mail = event.target.value;
+    localStorage.setItem(STORAGE_MAIL, mail)
 }
 function onFormSubmit(event) {
     event.preventDefault();
     event.target.reset();
-    localStorage.removeItem(STORAGE);
-    console.log(formData)
+    localStorage.removeItem(STORAGE_FORM);
+    localStorage.removeItem(STORAGE_MAIL);
+    console.log(formData);
 }
 function savedLocal() {
-    const savedMessage = localStorage.getItem(STORAGE);
+    const savedMessage = localStorage.getItem(STORAGE_FORM);
+    const savedMail = localStorage.getItem(STORAGE_MAIL);
     if (savedMessage) {
-        console.log(savedMessage);
         refs.textarea.value = savedMessage;
+    }
+    if (savedMail) {
+        refs.mail.value = savedMail;
     }
     
 }
